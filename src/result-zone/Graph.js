105a9c6b5492e228053329c4ad1865a83f936d28
Maria Terenteva <rtillkriyt@gmail.com>
2021-07-18 00:33:51 +0300

    design creation

--- src/result-zone/Graph.js
+++ src/result-zone/Graph.js
@@ -4,9 +4,21 @@ import React from 'react';
 function Graph(props) {
   
     return (
-        <>
- 
-        </>
+        <div className="graph">
+            <h2 className="graph__temperature">Temperature</h2>
+            <div className="graph__data">
+
+            </div>
+            <div className="week">
+                <div className="day">
+                    <p className="day___name">Today</p>
+                    <img className="day___weather-graph"/>
+                    <p className="humidity__name humidity__name_day">Humidity</p>
+                    <p className="humidity__percent humidity__percent_day">30%</p>
+                </div>
+
+            </div>
+        </div>
 
     );
 
--- src/result-zone/Temperature.js
+++ src/result-zone/Temperature.js
@@ -4,16 +4,22 @@ import React from 'react';
 function Temperature(props) {
   
     return (
-        <>
+        <div className="temperature">
             <p className="date">5:05 PM, Mon, Nov 23, 2020</p>
-            <img/>
+            <img className="temperature_image"/>
             <h1 className="degrees">72 F</h1>
             <h2 className="weather-kind">Cloudy</h2>
-            <p className="humidity">Humidity</p>
-            <p className="wind-speed">Wind speed</p>
-            <p className="humidity-percent">45%</p>
-            <p className="wind-speed-num">19.2 km/j</p>
-        </>
+            <div className="humidity-wind-speed">
+                <div className="humidity">
+                    <p className="humidity__name">Humidity</p>
+                    <p className="humidity__percent">45%</p>
+                </div>
+                <div className="wind-speed">
+                    <p className="wind-speed__name">Wind speed</p>
+                    <p className="wind-speed__numbers">19.2 km/j</p>
+                </div>
+            </div>
+        </div>
 
     );
 
